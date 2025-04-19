816c506389901dbf527b4973d920a6cbe89b645e
Parth5409 <parthgaikwad5409@gmail.com>
2025-04-19 13:01:50 +0530

    Added Dashboard and Projects Page

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,29 +1,7 @@
-<<<<<<< HEAD
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
-      },
-    ]
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
->>>>>>> d812cb3a95a13467e6bda960111af56fa9bbfd86
 };
 
 export default nextConfig;
--- Frontend/postcss.config.mjs
+++ Frontend/postcss.config.mjs
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
-=======
-/** @type {import('postcss-load-config').Config} */
-export default {
-  plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> d812cb3a95a13467e6bda960111af56fa9bbfd86
